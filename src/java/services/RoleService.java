6fdbff01c43b338df1b9380e8630aab819788903
RT <RT@FLUBB>
2022-10-28 04:09:31 -0600

    Added Code Coded added to userdb.sql, User.java, ConnectionPool.java, DBUtil.java

--- src/java/services/RoleService.java
+++ src/java/services/RoleService.java
@@ -14,32 +14,5 @@ import java.io.Serializable;
  */
 public class RoleService implements Serializable {
     
-    public static final String PROP_SAMPLE_PROPERTY = "sampleProperty";
-    
-    private String sampleProperty;
-    
-    private PropertyChangeSupport propertySupport;
-    
-    public RoleService() {
-        propertySupport = new PropertyChangeSupport(this);
-    }
-    
-    public String getSampleProperty() {
-        return sampleProperty;
-    }
-    
-    public void setSampleProperty(String value) {
-        String oldValue = sampleProperty;
-        sampleProperty = value;
-        propertySupport.firePropertyChange(PROP_SAMPLE_PROPERTY, oldValue, sampleProperty);
-    }
-    
-    public void addPropertyChangeListener(PropertyChangeListener listener) {
-        propertySupport.addPropertyChangeListener(listener);
-    }
-    
-    public void removePropertyChangeListener(PropertyChangeListener listener) {
-        propertySupport.removePropertyChangeListener(listener);
-    }
     
 }
